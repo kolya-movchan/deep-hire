a5f95abe3b2cdfbe04bfc065b0e932e7d2b34db0
Kolya Movchan <kmovchan261@gmail.com>
2024-12-15 17:45:12 +0200

    fix: resolve import order error

--- src/@components/components/ui/avatar.tsx
+++ src/@components/components/ui/avatar.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
+import { cn } from "@components/lib/utils"
 import * as AvatarPrimitive from "@radix-ui/react-avatar"
 
-import { cn } from "@components/lib/utils"
 
 const Avatar = React.forwardRef<
   React.ElementRef<typeof AvatarPrimitive.Root>,

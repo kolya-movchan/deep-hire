2387a56fe701052a346e16089f32e892ab12f9d3
Kolya Movchan <kmovchan261@gmail.com>
2024-12-15 23:57:04 +0200

    test/prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,10 @@
 import "./App.css"
-import {
-  Avatar,
-  AvatarImage,
-  AvatarFallback,
-} from "@components/components/ui/avatar"
+import { Avatar, AvatarImage, AvatarFallback } from "@components/components/ui/avatar"
 
 function App() {
   return (
     <Avatar className="w-16 h-16 bg-gray-200">
       <AvatarImage src="https://github.com/shadcn.png" />
-
-      <div></div>
 
       <AvatarFallback>CN</AvatarFallback>
     </Avatar>

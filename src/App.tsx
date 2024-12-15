e6a150b61ddd8791b7fcf419be6c2e9062a5df8c
Kolya Movchan <kmovchan261@gmail.com>
2024-12-15 17:41:04 +0200

    fix: remove default react setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,17 @@
-import { useState } from "react";
-import viteLogo from "/vite.svg";
 import {
   Avatar,
   AvatarImage,
   AvatarFallback,
 } from "@components/components/ui/avatar";
-import reactLogo from "./assets/react.svg";
+
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <Avatar className="w-16 h-16 bg-gray-200">
-        <AvatarImage src="https://github.com/shadcn.png" />
-        <AvatarFallback>CN</AvatarFallback>
-      </Avatar>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Avatar className="w-16 h-16 bg-gray-200">
+      <AvatarImage src="https://github.com/shadcn.png" />
+      <AvatarFallback>CN</AvatarFallback>
+    </Avatar>
   );
 }
 

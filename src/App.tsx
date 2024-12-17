7ad3ca5cd1e7a88c058ddaa0c4cfb14450d29127
Kolya Movchan <kmovchan261@gmail.com>
2024-12-17 23:51:40 +0200

    feat: add redux toolkit

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
-import "./App.css"
 import { Avatar, AvatarImage, AvatarFallback } from "@components/components/ui/avatar"
+import { Button } from "@components/components/ui/button"
+import "./App.css"
 
 function App() {
   return (
-    <Avatar className="w-16 h-16 bg-gray-200">
-      <AvatarImage src="https://github.com/shadcn.png" />
+    <>
+      <Avatar className="w-16 h-16 bg-gray-200">
+        <AvatarImage src="https://github.com/shadcn.png" />
 
-      <AvatarFallback>CN</AvatarFallback>
-    </Avatar>
+        <AvatarFallback>CN</AvatarFallback>
+      </Avatar>
+
+      <Button>Click me</Button>
+    </>
   )
 }
 

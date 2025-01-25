991fb347a10fa7c5ca8b673f075e4e4864f81b2a
Kolya Movchan <kmovchan261@gmail.com>
2025-01-25 12:31:57 +0200

    feat: add amplify and graphql

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,20 @@
+export declare type AmplifyDependentResourcesAttributes = {
+  auth: {
+    aiflex: {
+      IdentityPoolId: string
+      IdentityPoolName: string
+      UserPoolId: string
+      UserPoolArn: string
+      UserPoolName: string
+      AppClientIDWeb: string
+      AppClientID: string
+    }
+  }
+  api: {
+    aiflex: {
+      GraphQLAPIKeyOutput: string
+      GraphQLAPIIdOutput: string
+      GraphQLAPIEndpointOutput: string
+    }
+  }
+}

22b05c41469e6fe8e09dbcbdd123f6240905abc8
Kolya Movchan <kmovchan261@gmail.com>
2025-02-17 09:28:38 +0200

    feat: make GraphQL verification via Cognito

--- src/api/graphql/client.ts
+++ src/api/graphql/client.ts
@@ -1,10 +1,29 @@
-import { ApolloClient, InMemoryCache } from "@apollo/client"
+import { ApolloClient, InMemoryCache, createHttpLink } from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
+import { getToken } from "../rest/auth"
 
-const client = new ApolloClient({
+// Create the http link
+const httpLink = createHttpLink({
   uri: import.meta.env.VITE_APP_SYNC_URL,
-  headers: {
-    "x-api-key": import.meta.env.VITE_APP_SYNC_API_KEY,
-  },
+})
+
+// Create the auth link
+const authLink = setContext(async (_, { headers }) => {
+  // Get the token on each request
+  const token = await getToken()
+
+  return {
+    headers: {
+      ...headers,
+      "Content-Type": "application/json",
+      Authorization: token ? `Bearer ${token}` : "", // Added Bearer prefix
+    },
+  }
+})
+
+// Create the Apollo Client with the auth link
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 

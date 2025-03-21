a4b2b832ef68db826a19bac3c70f87457530cacf
Kolya Movchan <kmovchan261@gmail.com>
2025-03-21 09:00:48 +0200

    refactor: remove unused imports

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,7 @@
 import { configureStore } from "@reduxjs/toolkit"
 import authReducer from "./auth-slice"
-import { AuthState } from "@/types/state"
 import creditsReducer from "./credits-slice"
-import { CreditsState } from "@/types/state"
 import visitorReducer from "./visitor-slice"
-import { VisitorState } from "@/types/state"
 import candidateAnalysesReducer from "./candidate-analyses-slice"
 import candidateDetailsReducer from "./candidate-details-slice"
 

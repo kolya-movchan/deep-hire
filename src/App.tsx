e080f38f5e55d043ba085bbc25ac0b68e817af97
Kolya Movchan <kmovchan261@gmail.com>
2025-09-21 11:33:38 +0300

    fix: use prev file name upper cased

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,22 @@ import { Provider } from "react-redux"
 import { store } from "./store"
 import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from "./routes/app-routes"
-// import { FpjsProvider } from "@fingerprintjs/fingerprintjs-pro-react" // Temporarily disabled FingerprintJS for access
+import { FpjsProvider } from "@fingerprintjs/fingerprintjs-pro-react"
 
 function App() {
   return (
-    // <FpjsProvider
-    //   loadOptions={{
-    //     apiKey: import.meta.env.VITE_FINGERPRINT_API_KEY,
-    //     region: "eu",
-    //   }}
-    // >
-    <Provider store={store}>
-      <BrowserRouter>
-        <AppRoutes />
-      </BrowserRouter>
-    </Provider>
-    // </FpjsProvider>
-    // FingerprintJS temporarily disabled - re-enable when needed
+    <FpjsProvider
+      loadOptions={{
+        apiKey: import.meta.env.VITE_FINGERPRINT_API_KEY,
+        region: "eu",
+      }}
+    >
+      <Provider store={store}>
+        <BrowserRouter>
+          <AppRoutes />
+        </BrowserRouter>
+      </Provider>
+    </FpjsProvider>
   )
 }
 
